487526a85b45a67b2b4cc3acacaed8ea9b28dc07
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-01-18 11:18:23 -0500

    Feat(header-nav): added header-nav using MUI

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+
+import Header from "../organisms/header";
 import Layout from "../templates/layout";
+import Menu from "../organisms/menu";
+import InputSearch from "../organisms/search";
+
 
 function App() {
   return (
     <div>
-      <Layout />
+      <Layout> 
+        <Header />
+        <Menu />
+        <InputSearch />
+        
+      </Layout>
     </div>
   );
 }
--- src/templates/layout.jsx
+++ src/templates/layout.jsx
@@ -1,20 +1,22 @@
 import {Box, CssBaseline, Grid} from '@mui/material'
 import { grey } from '@mui/material/colors';
 
-export default function Layout() {
-  return (
-      <Box style={{display: "flex"}}>
+export default function Layout({children}) {
+    const [header, menu, search, premium] = children;
+
+    return (
+        <Box style={{display: "flex", height: "100vh"}}>
             <CssBaseline/>
             <Grid container m={2}>
-                <Grid item flexBasis={120} bgcolor={grey[400]} height= {"90vh"} m={2}>NAVIGATION</Grid>
+                <Grid item flexBasis={260}>{menu}</Grid>
                 <Grid item flex={2}>
-                    <Grid xs={12} bgcolor={grey[400]} height={'20vh'} my={2}>HEADER</Grid>
+                    <Grid xs={12}>{header}</Grid>
                     <Grid xs={12} bgcolor={grey[400]} height={'20vh'} my={2}>USER INPUT</Grid>
                     <Grid xs={12} bgcolor={grey[400]} height={'45vh'}>THREAD</Grid>
                 </Grid> 
                 <Grid item flex={1} m={2}>
-                    <Grid xs={12} bgcolor={grey[400]} height={'10vh'}>SEARCH</Grid>
-                    <Grid xs={12} bgcolor={grey[400]} height={'20vh'} my={2}>CONTAINER 1</Grid>
+                    <Grid xs={12}>{search}</Grid>
+                    <Grid xs={12} my={2}>{premium}</Grid>
                     <Grid xs={12} bgcolor={grey[400]} height={'30vh'} mb={2}>CONTAINER 2</Grid>
                     <Grid xs={12} bgcolor={grey[400]} height={'22vh'}>CONTAINER 3</Grid>
                 </Grid>
